5f9bed414661a839a49ef36ec2743fa082c6309b
Richard Osterloh <richard.osterloh@gmail.com>
2019-09-01 18:30:41 +0100

    soc: arm: st_stm32: Add STM32G4 SoC series
    Add soc and dts files to support for most of the common peripherals
    in the STM32G4 series. Add specific support for the STM32G431RB.
    
    Signed-off-by: Richard Osterloh <richard.osterloh@gmail.com>

---
+++ soc/arm/st_stm32/stm32g4/soc.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2019 Richard Osterloh <richard.osterloh@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file SoC configuration macros for the STM32G4 family processors.
+ *
+ * Based on reference manual:
+ *   STM32G4xx advanced ARM ® -based 32-bit MCUs
+ *
+ * Chapter 2.2: Memory organization
+ */
+
+
+#ifndef _STM32G4_SOC_H_
+#define _STM32G4_SOC_H_
+
+#ifndef _ASMLANGUAGE
+
+#include <autoconf.h>
+#include <stm32g4xx.h>
+
+/* ARM CMSIS definitions must be included before kernel_includes.h.
+ * Therefore, it is essential to include kernel_includes.h after including
+ * core SOC-specific headers.
+ */
+#include <kernel_includes.h>
+
+#endif /* !_ASMLANGUAGE */
+
+#endif /* _STM32G4_SOC_H_ */
