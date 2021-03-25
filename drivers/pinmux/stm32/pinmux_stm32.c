fc76f1f65ca7928fd745c60905d57a9a81e88bb8
Erwan Gouriou <erwan.gouriou@linaro.org>
2021-03-25 17:29:30 +0100

    drivers/pinmux: stm32: Enable PM_DEVICE_IDLE services
    Implement PM_DEVICE_IDLE using a workaround.
    Calling device_pm primitives causes issues when running
    tests/drivers/gpio.
    Limitation: Prevents use of pinmux in PM_DEVICE_IDLE mode
    in devices drivers.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

--- drivers/pinmux/stm32/pinmux_stm32.c
+++ drivers/pinmux/stm32/pinmux_stm32.c
@@ -127,15 +127,27 @@ int stm32_dt_pinctrl_configure(const struct soc_gpio_pinctrl *pinctrl,
 			       STM32_DT_PINMUX_LINE(mux));
 		port_device = gpio_ports[STM32_PORT(pin)];
 
+#ifdef CONFIG_PM_DEVICE_IDLE
+		ret = device_pm_get_sync(port_device);
+#else
 		ret = gpio_stm32_clock_request(port_device, true);
+		/* Note, we don't use pm_constraint_foo functions here */
+		/* since idle period should not happen between clock_on */
+		/* and clock_off */
+#endif
+
 		if (ret != 0) {
 			return ret;
 		}
 
 		stm32_pin_configure(pin, func, STM32_DT_PINMUX_FUNC(mux));
+
+#ifdef CONFIG_PM_DEVICE_IDLE
+		ret = device_pm_put(port_device);
+#endif
 	}
 
-	return 0;
+	return ret;
 }
 
 #if DT_HAS_COMPAT_STATUS_OKAY(st_stm32f1_pinctrl)
